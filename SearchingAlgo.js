a0f129055a7c6429385b77c3a63cfc5f77372b98
amaben2020 <amaben202phantom@gmail.com>
2021-04-18 15:18:19 +0100

    Searching Algo Binary search Solution update..

--- SearchingAlgo.js
+++ SearchingAlgo.js
@@ -10,9 +10,18 @@ console.log(linearSearch([4, 14, 21, 3, 44], 21));
 const binarySearch = (array, value) => {
 	let rightPointer = array.length - 1;
 
-	let leftPointer = array[0];
+	let leftPointer = 0;
 
 	let middlePointer = Math.floor((rightPointer + leftPointer) / 2);
+
+	while (value !== array[middlePointer]) {
+		if (value < array[middlePointer]) {
+			rightPointer = middlePointer - 1;
+			console.log(rightPointer);
+		}
+		middlePointer = Math.floor((rightPointer + leftPointer) / 2);
+	}
+	return middlePointer;
 };
 
-console.log(binarySearch([4, 14, 21, 3, 44], 21));
+console.log(binarySearch([4, 14, 21, 66, 3, 44], 21));
