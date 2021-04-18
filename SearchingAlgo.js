fcc1dd6b7963e2a38cab1fcd0673bc0a3f7ca04a
amaben2020 <amaben202phantom@gmail.com>
2021-04-18 14:58:05 +0100

    Searching Algo linear search Solution update..

--- SearchingAlgo.js
+++ SearchingAlgo.js
@@ -1,9 +1,8 @@
 const linearSearch = (array, value) => {
 	for (let i = 0; i < array.length; i++) {
-		if (value === array[i]) {
-			return i++;
-		}
+		if (value === array[i]) return i++;
 	}
+	return "Not found";
 };
 
-console.log(linearSearch([4, 14, 21, 3, 44], 10));
+console.log(linearSearch([4, 14, 21, 3, 44], 21));
