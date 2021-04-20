1572df136dfb3ef13bcc9f5c3e62639a50c3e56c
amaben2020 <amaben202phantom@gmail.com>
2021-04-20 17:26:05 +0100

    Techniques of solving algorithms.

--- Sorting.js
+++ Sorting.js
@@ -59,11 +59,12 @@ const charC = (str) => {
 
 console.log(charC(charCount));
 
+//ANAGRAMS: Frequency counter technique
 const anagrams = (str1, str2) => {
 	const lookup = {};
 
 	for (let i = 0; i < str1.length; i++) {
-		let string1 = str1[i];
+		const string1 = str1[i];
 
 		if (lookup[string1]) {
 			lookup[string1]++;
@@ -71,16 +72,51 @@ const anagrams = (str1, str2) => {
 			lookup[string1] = 1;
 		}
 	}
-
 	for (let j = 0; j < str2.length; j++) {
-		let string2 = str2[j];
+		const string2 = str2[j];
+
 		if (!lookup[string2]) {
 			return false;
 		} else {
-			lookup[string2] -= 1;
+			lookup[string2]--;
 		}
 	}
+	console.log(lookup);
 	return true;
 };
 
-console.log(anagrams("anagram", "angrawwma"));
+console.log(anagrams("anagram", "angrama"));
+
+//write a program that adds 2 values and gives zero
+//USING MULTIPLE POINTER TECHNIQUE
+const sumZero = (array) => {
+	let left = 0;
+	let right = array.length - 1;
+
+	while (left < right) {
+		let sum = array[left] + array[right];
+		console.log(sum);
+		if (sum === 0) {
+			return [array[left], array[right]];
+		} else if (sum > 0) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+};
+
+console.log(sumZero([-1, 2, 3, 4, 5, 12, 1, 13]));
+
+function solve(meal_cost, tip_percent, tax_percent) {
+	meal_cost = 12.0;
+
+	tip_percent = (meal_cost / 100) * 20;
+
+	tax_percent = (meal_cost / 100) * 8;
+
+	const total_cost = meal_cost + tip_percent + tax_percent;
+
+	return Math.round(total_cost);
+}
+console.log(solve(12, 20, 8));
